8a1696a5ac969a488d11ee34eabd0f3b74ef827e
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-02-23 19:53:56 -0800

    finished 11.3

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
+
 
 const {animals} = require('./data/animals');
 
@@ -113,7 +115,17 @@ app.get('/api/animals', (req, res) => {
       }
   });
 
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`)
